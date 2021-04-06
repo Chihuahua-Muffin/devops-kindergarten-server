01842046d8136fa922a5d29383af77eee2e8ee4d
SeoSeunghun <proto_seo@naver.com>
2021-04-06 22:04:08 +0900

    first commit

---
+++ src/test/java/devops/kindergarten/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package devops.kindergarten.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
