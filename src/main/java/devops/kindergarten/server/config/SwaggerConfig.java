553ea035ed0b36d7d94c7bb4d110eb320a3b4789
SeoSeunghun <proto_seo@naver.com>
2021-05-25 15:53:05 +0900

    swagger 추가 및 설정 추가 #1

---
+++ src/main/java/devops/kindergarten/server/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package devops.kindergarten.server.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Demo")
+                .description("API EXAMPLE")
+                .build();
+    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("swg-group1")//빈설정을 여러개 해줄경우 구분하기 위한 구분자.
+                .apiInfo(this.apiInfo())//스웨거 설명
+                .select()//apis, paths를 사용하주기 위한 builder
+                .apis(RequestHandlerSelectors.basePackage("com.sgw.web.controller"))//탐색할 클래스 필터링
+                .paths(PathSelectors.any())//스웨거에서 보여줄 api 필터링
+                .build();
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        //enabling swagger-ui part for visual documentation
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
